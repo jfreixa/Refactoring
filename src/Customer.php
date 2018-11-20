3844e7d720f17535660d5b32b32c8ee17891559e
Jordi Freixa <jordi.freixa@atrapalo.com>
2018-11-20 16:56:30 +0100

    Extract method

--- src/Customer.php
+++ src/Customer.php
@@ -26,19 +26,26 @@ class Customer
 
     public function statement(): string
     {
-        $totalAmount = 0;
         $frequentRenterPoints = 0;
         $result = "Rental Record for ".$this->name()."\n";
         foreach ($this->rentals as $rental) {
             $frequentRenterPoints += $rental->frequentRenterPoints();
             $result .= "\t".$rental->movie()->title()."\t".$rental->charge()."\n";
-            $totalAmount += $rental->charge();
         }
 
-        $result .= "Amount owed is ".$totalAmount."\n";
+        $result .= "Amount owed is ".$this->totalAmount()."\n";
         $result .= "You earned ".$frequentRenterPoints." frequent renter points";
 
         return $result;
     }
 
+    private function totalAmount()
+    {
+        $totalAmount = 0;
+        foreach ($this->rentals as $rental) {
+            $totalAmount += $rental->charge();
+        }
+
+        return $totalAmount;
+    }
 }
